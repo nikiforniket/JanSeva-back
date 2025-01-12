ff3c6c76ac30903e9bda522faf80d7cfeeabe8df
nikiforniket <nikeetkumar7857@gmail.com>
2025-01-12 19:42:39 +0530

    added location admin files

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,39 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 
-# Register your models here.
+from location.models import State, Constituency, Block, LocalBody, SubLocalBody
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ("name", "code")
+    search_fields = ("name", "code")
+    list_filter = ("name",)
+
+
+@admin.register(Constituency)
+class ConstituencyAdmin(admin.ModelAdmin):
+    list_display = ("no", "state", "name", "election_type")
+    list_filter = ("state", "election_type")
+    search_fields = ("name", "state__name")
+
+
+@admin.register(LocalBody)
+class LocalBodyAdmin(admin.ModelAdmin):
+    list_display = ("name", "local_body_type", "block")
+    list_filter = ("local_body_type", "block")
+    search_fields = ("name",)
+
+
+@admin.register(Block)
+class BoothAdmin(admin.ModelAdmin):
+    list_display = ("name", "constituency")
+    list_filter = ("constituency",)
+    search_fields = ("name",)
+
+
+@admin.register(SubLocalBody)
+class SubLocalBodyAdmin(admin.ModelAdmin):
+    list_display = ("name", "local_body")
+    search_fields = ("name",)
--- location/models/state.py
+++ location/models/state.py
@@ -8,6 +8,7 @@ from common.models import TimestampedMetaModelMixin
 class State(TimestampedMetaModelMixin):
 
     name = models.CharField(max_length=100)
+    code = models.CharField(max_length=4, unique=True)
 
     def __str__(self):
         return f"{self.name}"
